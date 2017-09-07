8ea09d5c418d0e32d78e28a103dff2973b0705f3
kit <catslove28@gmail.com>
2017-09-07 16:47:43 +0800

    reapir route & component bug

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import Router from 'vue-router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import Hello from '@/components/Index'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import Index from '@/components/Index'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 Vue.use(Router){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+      name: 'index',
+      component: Index{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
     }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
   ]{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
