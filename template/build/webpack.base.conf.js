0f420fd89ad8adc93a8e4b550c9598d87b5840c5
Kit <kitwang@kitwang.local>
2018-03-15 16:06:26 +0800

    oridinary update

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -3,7 +3,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
-var entries = require('./get-entry').getEntry(resolve('src/modules/*/*.js'))
+const entries = require('./get-entry').getEntry(resolve('src/modules/*/*.js'))
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
